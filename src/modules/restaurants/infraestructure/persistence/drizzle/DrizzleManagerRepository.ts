a3e389ba0f5d8474f7867bf52a7d1ba1c656b4f6
David Azeredo <davispc100@gmail.com>
2024-08-06 22:56:58 +0000

    feat: create injection restaurantModel

--- src/modules/restaurants/infraestructure/persistence/drizzle/DrizzleManagerRepository.ts
+++ src/modules/restaurants/infraestructure/persistence/drizzle/DrizzleManagerRepository.ts
@@ -4,13 +4,18 @@ import type { RestaurantRepository } from '../../../application/port/out/Restaur
 import { Restaurant } from '../../../domain/restaurant/Restaurant';
 
 class DrizzleRestaurantRepository implements RestaurantRepository {
+  constructor(private readonly restaurantModel = restaurants) {}
+
   async create(restaurant: Restaurant): Promise<Restaurant> {
     const { name, description, managerId } = restaurant.getProps();
-    const [restaurantStored] = await db.insert(restaurants).values({
-      name,
-      description,
-      managerId
-    }).returning();
+    const [restaurantStored] = await db
+      .insert(restaurants)
+      .values({
+        name,
+        description,
+        managerId
+      })
+      .returning();
     return new Restaurant(restaurantStored);
   }
 
