b3f770f4d0a36db6b00ad4c647b470bff96553fb
David Azeredo <davispc100@gmail.com>
2024-08-15 00:50:10 +0000

    fix: add reflect and config drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'drizzle-kit'
 import { env } from './src/env';
 
 export default {
-  schema: './src/db/drizzle/schema/index.ts',
-  out: './src/db/drizzle/sql',
+  schema: './src/shared/infraestructure/persistence/drizzle/schema/index.ts',
+  out: './src/shared/infraestructure/persistence/drizzle/sql',
   dialect: 'postgresql',
   dbCredentials: {
     url: env.DATABASE_URL,
